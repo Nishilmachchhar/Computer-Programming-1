abd9eeff349d6c45bff00114bfdd5eb8bc24ba9d
Nishilmachchhar <nishilmachchhar@gmail.com>
2025-08-12 18:08:14 +0530

    Add files via upload

---
+++ Nihsil.c
@@ -0,0 +1,18 @@
+// online c compiler to run c program online
+#include <stdio.h>
+int main()
+{
+    int a,b;
+  printf("Enter number1\n");
+
+  scanf("%d""%d",&a,&b);
+  if(a>b)
+  {
+    printf("a is the largest value");
+  }
+    else
+    {
+    printf("bis the largest value");
+    }
+    return 0;
+    }
