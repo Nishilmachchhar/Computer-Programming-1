40afc3f9a1c4e3d928b2ae471ce47ca970b5f7bf
Nishilmachchhar <nishilmachchhar@gmail.com>
2025-08-13 22:12:01 +0530

    Add files via upload

---
+++ Nishil C programming(Rupees into dollars) 9 C.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float rupees;
+    printf("Enter Rupees: ");
+    scanf("%f", &rs);
+    printf("%.2f Rs = %.2f dollars\n", rupees, rupees / 48);
+    return 0;
+}
